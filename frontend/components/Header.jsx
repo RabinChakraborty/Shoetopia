70318b67a28dd5f14cbc0bf8c007d1b8df19f703
Rabin Chakraborty <rabin747809@gmail.com>
2023-04-19 22:29:16 +0530

    Storing all pictures that i think i might need

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Wrapper from './Wrapper';
+import Link from 'next/link';
+import Menu from './Menu';
 
 const Header = () => {
-    return <div className="text">Header</div>;
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState('translate-y-0');
+  const [lastScrolly, setLastScrolly] = useState(0);
+
+  return (
+    <div
+      className={` w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper>
+        <Link href='/'>
+          <img
+            src='/Main-logo.ico'
+            alt='main loogo'
+            className='w-[40px] md:w-[60px] '
+          />
+        </Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Header;
